40a65d22bbdaf3b6e573a8e332225358ca1f7821
JesusFranC <jesusfranciscocerda@gmail.com>
2023-10-11 17:17:29 -0700

    Jesus Cerda Files Update

---
+++ Project Source Code/Database_Logging/Jesus/TeamSpecs.Ridealong/TeamSpecs.Ridealong.ConsoleApp/ConsoleApp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data.SqlClient;
+using TeamSpecs.Ridealong.DataAccess;
+using TeamSpecs.Ridealong.Logging;
+
+namespace TeamSpecs.RideAlong.ConsoleApp
+{
+    public class ConsoleApp
+    {
+        static void Main(string[] args)
+        {
+            Logging logs = new Logging();
+
+            bool invalid = true;
+            string message = "";
+            Console.WriteLine("What message do you want to send (maximum 50 characters)");
+            while (invalid)
+            {
+                message += Console.ReadLine();
+                if (message.Length < 100 && message != "")
+                {
+                    invalid = false;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid argument");
+                    message = "";
+                }
+            }
+            logs.writeToDataStore(message);
+
+            DataAccess data = new DataAccess();
+            data.read("SELECT * FROM LoggingTestTable;");
+        }
+    }
+}
