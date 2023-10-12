501acc97e262fea092291b1fafaa69809aceabd2
gioman13 <143643519+gioman13@users.noreply.github.com>
2023-10-11 17:16:12 -0700

    Gio Database assignment

---
+++ Project Source Code/Database_Logging/Gio/Specs.RideAlong/Specs.RideAlong.DataAcceess/DataAccess.cs
@@ -0,0 +1,53 @@
+﻿using System.Data.SqlClient;
+
+namespace Specs.RideAlong.DataAccess
+{
+    public class DataAccess
+    {
+        private string connectionString = @"Server=DESKTOP-VUMHD7S;Database=RideAlongdb;";
+        private string adminString = "User Id=RideAlongAdmin;Password=admin;";
+        private string readString = "User Id=RideAlongRead;Password=readme;";
+        private string writeString = "User Id=RideAlongWrite;Password=writeme;";
+
+        public void write(string querry)
+        {
+            SqlConnection writeConnection = new SqlConnection(connectionString + writeString);
+            SqlCommand command;
+
+            writeConnection.Open();
+            command = writeConnection.CreateCommand();
+            command.CommandText = querry;
+            command.ExecuteNonQuery();
+            writeConnection.Close();
+        }
+
+        public void read(string querry)
+        {
+            SqlConnection readConnection = new SqlConnection(connectionString + readString);
+            SqlCommand command;
+
+            readConnection.Open();
+            command = readConnection.CreateCommand();
+            command.CommandText = querry;
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine("{0} {1} {2}", reader["LogID"].ToString(), reader["logTime"].ToString(), reader["logMessage"].ToString());
+            }
+            readConnection.Close();
+
+        }
+
+        public void admin(string querry)
+        {
+            SqlConnection adminConnection = new SqlConnection(connectionString + adminString);
+            SqlCommand command;
+
+            adminConnection.Open();
+            command = adminConnection.CreateCommand();
+            command.CommandText = querry;
+            command.ExecuteNonQuery();
+            adminConnection.Close();
+        }
+    }
+}
