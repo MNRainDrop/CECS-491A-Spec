c8fa8fcd879d98715324b2f29b8e172f7b5c731f
MNRainDrop <79126834+MNRainDrop@users.noreply.github.com>
2023-10-11 17:11:10 -0700

    Rainier's Assignment

---
+++ Project Source Code/Database_Logging/Rainier/TeamSpecs.RideAlong/TeamSpecs.RideAlong.ConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data.SqlClient;
+using TeamSpecs.RideAlong.DataAccess;
+using TeamSpecs.RideAlong.Logging;
+
+namespace TeamSpecs.RideAlong.ConsoleApp
+{
+    public class ConsoleApp
+    {
+        static void Main(string[] args)
+        {
+            Logging.Logging logs = new Logging.Logging();
+
+            bool invalid = true;
+            string message = "";
+            Console.WriteLine("What message do you want to send (maximum 50 characters)");
+            while (invalid)
+            {
+                message += Console.ReadLine();
+                if (message.Length < 100 && message != "")
+                {
+                    invalid = false;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid argument");
+                    message = "";
+                }
+            }
+            logs.writeToDataStore(message);
+
+            DataAccess.DataAccess data = new DataAccess.DataAccess();
+            data.read("SELECT * FROM LoggingTestTable;");
+        }
+    }
+}
