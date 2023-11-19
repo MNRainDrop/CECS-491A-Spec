6f52a7f2a8ffd62c79a54ba3aaf7b53863ac48ef
MNRainDrop <79126834+MNRainDrop@users.noreply.github.com>
2023-11-18 17:36:07 -0800

    Uploading everything

--- Project_Source_Code/RainiersCode/TeamSpecs.RideAlong/TeamSpecs.RideAlong.LoggingLibrary/LogService.cs
+++ Project_Source_Code/RainiersCode/TeamSpecs.RideAlong/TeamSpecs.RideAlong.LoggingLibrary/LogService.cs
@@ -9,11 +9,11 @@ public class LogService : ILogService
         _logTarget = logTarget;
     }
 
-    public Response Log(string logLevel, string category, string? message)
+    public Response Log(string logLevel, string logCategory, string? logMessage)
     {
         var response = new Response();
         // Not sure if the startTime parameter should be the current time of the log being logged or the time the user operation should be 
-        response = _logTarget.Write(DateTime.UtcNow, logLevel, category, message);
+        response = _logTarget.Write(DateTime.UtcNow, logLevel, logCategory, logMessage);
         return response;
     }
 }
