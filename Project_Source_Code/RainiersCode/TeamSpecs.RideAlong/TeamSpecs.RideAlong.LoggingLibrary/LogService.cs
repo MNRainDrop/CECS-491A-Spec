9e3cabc75b4b198d843c3595fe01adbba89ec031
JesusFranC <94206855+JesusFranC@users.noreply.github.com>
2023-11-18 19:45:56 -0800

    Adding Log class, and interfac

--- Project_Source_Code/RainiersCode/TeamSpecs.RideAlong/TeamSpecs.RideAlong.LoggingLibrary/LogService.cs
+++ Project_Source_Code/RainiersCode/TeamSpecs.RideAlong/TeamSpecs.RideAlong.LoggingLibrary/LogService.cs
@@ -12,7 +12,10 @@ public class LogService : ILogService
     public IResponse Log(string logLevel, string logCategory, string? logMessage)
     {
         // Not sure if the startTime parameter should be the current time of the log being logged or the time the user operation should be 
-        var response = _logTarget.Write(DateTime.UtcNow, logLevel, logCategory, logMessage);
+
+        //changed to work with log object
+        ILog log = new Log(null, DateTime.UtcNow, logLevel, logCategory, logMessage);
+        response = _logTarget.Write(log);
         return response;
     }
 }
--- Project_Source_Code/RainiersCode/TeamSpecs.RideAlong/TeamSpecs.RideAlong.LoggingLibrary/Targets/SqlDbLogTarget.cs
+++ Project_Source_Code/RainiersCode/TeamSpecs.RideAlong/TeamSpecs.RideAlong.LoggingLibrary/Targets/SqlDbLogTarget.cs
@@ -1,6 +1,6 @@
-﻿using TeamSpecs.RideAlong.Model;
+﻿using System.Data.SqlClient;
 using TeamSpecs.RideAlong.DataAccess;
-using System.Data.SqlClient;
+using TeamSpecs.RideAlong.Model;
 
 namespace TeamSpecs.RideAlong.LoggingLibrary;
 
@@ -13,19 +13,20 @@ public class SqlDbLogTarget : ILogTarget
         _dao = dao;
     }
 
-    public IResponse Write(DateTimeOffset startTime, string logLevel, string category, string? description)
+    public IResponse Write(ILog log)
     {
         // need to protect against sql injection
         // start time and end time I'm not 100% sure of - 11/13
 
         // removed the "logEndTime parameters because it is not necessary - 11/15
+        //Changed to work with log objects
         string sql = $@"INSERT INTO dbo.LoggingTestTable (logStartTime, logLevel, category, description)" +
             "VALUES (@logStartTime, @logLevel, @category, @description)";
         var cmd = new SqlCommand(sql);
-        cmd.Parameters.Add(new SqlParameter("@logStartTime", startTime));
-        cmd.Parameters.Add(new SqlParameter("@logLevel", logLevel));
-        cmd.Parameters.Add(new SqlParameter("@category", category));
-        cmd.Parameters.Add(new SqlParameter("@message", description));
+        cmd.Parameters.Add(new SqlParameter("@logStartTime", log.logTime));
+        cmd.Parameters.Add(new SqlParameter("@logLevel", log.logLevel));
+        cmd.Parameters.Add(new SqlParameter("@category", log.logCategory));
+        cmd.Parameters.Add(new SqlParameter("@message", log.logContext));
         return _dao.ExectueWriteOnly(cmd);
     }
 }
