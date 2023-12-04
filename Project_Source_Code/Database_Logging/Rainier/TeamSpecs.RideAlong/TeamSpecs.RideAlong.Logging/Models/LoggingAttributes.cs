c4102cd44ec7fc3805dd0b15132ccc4cf6ac9336
MNRainDrop <79126834+MNRainDrop@users.noreply.github.com>
2023-12-03 16:36:39 -0800

    Delete Project_Source_Code/Database_Logging directory
    Do not need multiple project files

--- Project_Source_Code/Database_Logging/Rainier/TeamSpecs.RideAlong/TeamSpecs.RideAlong.Logging/Models/LoggingAttributes.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TeamSpecs.RideAlong.Logging.Models
-{
-    public class LogAttributes
-    {
-        public int LogID { get; set; }
-        public DateTime Time { get; set; }
-        public string? Message { get; set; }
-    }
-}
