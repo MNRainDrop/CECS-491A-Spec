9dd4e1a3eef49dbd96abdf5b5af0c519f058c1b0
jason barber <jasonthegamer84@gmail.com>
2023-12-17 23:53:47 -0800

    Account Modification Test Cases

--- Project_Source_Code/TeamSpecs.RideAlong/TeamSpecs.RideAlong.UserAdministration/Interfaces/IAccountModificationService.cs
+++ Project_Source_Code/TeamSpecs.RideAlong/TeamSpecs.RideAlong.UserAdministration/Interfaces/IAccountModificationService.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TeamSpecs.RideAlong.Model;
 
 namespace TeamSpecs.RideAlong.UserAdministration
 {
     public interface IAccountModificationService
     {
-
+        IResponse ModifyUserProfie(string userName, DateTime dateOfBirth);
     }
 }
