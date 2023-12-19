12702b2689a21931d16af198baed4e569dcbe1df
JesusFranC <jesusfranciscocerda@gmail.com>
2023-12-18 18:23:27 -0800

    Update IAuthTarget.cs

--- Project_Source_Code/TeamSpecs.RideAlong/TeamSpecs.RideAlong.SecurityLibrary/Targets/IAuthTarget.cs
+++ Project_Source_Code/TeamSpecs.RideAlong/TeamSpecs.RideAlong.SecurityLibrary/Targets/IAuthTarget.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TeamSpecs.RideAlong.Model;
 
 namespace TeamSpecs.RideAlong.SecurityLibrary.Targets
 {
-    internal interface IAuthTarget
+    public interface IAuthTarget
     {
+        IResponse saveHashedPass(long UID, string passHash);//Returns a Response object with info as to if it passed or failed
+        IResponse getClaims(long UID);//Returns a Dict<string,string> of claims
+        IResponse fetchPass(long UID);//returns their password
+
     }
 }
