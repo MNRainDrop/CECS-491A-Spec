0cdd04c5528a26cf9d97706df9cc82dbfe75cf11
ViNguyen3 <80879169+ViNguyen3@users.noreply.github.com>
2024-03-01 18:13:29 -0800

    update pepper test cases
    update test cases to generate 10 peppers and retrieve 10 peppers

--- Project_Source_Code/TeamSpecs.RideAlong/TeamSpecs.RideAlong.Tests/PepperServiceShould.cs
+++ Project_Source_Code/TeamSpecs.RideAlong/TeamSpecs.RideAlong.Tests/PepperServiceShould.cs
@@ -14,18 +14,33 @@ namespace TeamSpecs.RideAlong.TestingLibrary
             var timer = new Stopwatch();
             IResponse response;
             var dao = new JsonFileDAO();
+            uint result;
             var _pepperTarget = new FilePepperTarget(dao);
             PepperService PepperObject = new PepperService(_pepperTarget);
             string key = "Test Key2";
+            //aray of 10 keys to be passed in for generate
+            string[] test_key = {"Test Key1", "Test Key2" , "Test Key3" , "Test Key4" , 
+            "Test Key5" , "Test Key6" , "Test Key7" , "Test Key8" , "Test Key9" , "Test Key10" };
+            List<uint> test_result = new List<uint>();
+
 
             //Act 
             timer.Start();
-            var result = PepperObject.GeneratePepper(key);
+            foreach (var i in test_key)
+            {
+                result = PepperObject.GeneratePepper(i); 
+                test_result.Add(result);
+            }
+            //result = PepperObject.GeneratePepper(key);
             timer.Stop();
 
             //Assert 
             Assert.True(timer.Elapsed.TotalSeconds <= 5);
-            Assert.True(result.GetType() == typeof(uint));
+            //Assert.True(result.GetType() == typeof(uint));
+            foreach (var i in test_result)
+            {
+                Assert.True(i.GetType() == typeof(uint));
+            }
         }
 
         [Fact]
@@ -55,25 +70,39 @@ namespace TeamSpecs.RideAlong.TestingLibrary
         public void PepperService_RetrievePepper_KeyPassedIn_ReturnValue_Pass()
         {
             //Arrange 
+            uint result;
             var timer = new Stopwatch();
             IResponse response;
             var dao = new JsonFileDAO();
             var _pepperTarget = new FilePepperTarget(dao);
             PepperService PepperObject = new PepperService(_pepperTarget);
-            string key = "Test Key2";
-            uint value = 3490748009;
+            string key = "Test Key5";
+            uint value = 1161839200;
+            //aray of 10 keys to be passed in for retrieving 
+            string[] test_key = {"Test Key1", "Test Key2" , "Test Key3" , "Test Key4" ,
+            "Test Key5" , "Test Key6" , "Test Key7" , "Test Key8" , "Test Key9" , "Test Key10" };
+            List<uint> test_result = new List<uint>();
 
             //Act 
             timer.Start();
-            var result = PepperObject.RetrievePepper(key);
+            //var result = PepperObject.RetrievePepper(key);
+            foreach (var i in test_key)
+            {
+                result = PepperObject.RetrievePepper(i);
+                test_result.Add(result);
+            }
             timer.Stop();
 
             //Assert 
             Assert.True(timer.Elapsed.TotalSeconds <= 3);
-            Assert.True(result==value);
-
+            foreach (var i in test_result)
+            {
+                Assert.True(i.GetType() == typeof(uint));
+            }
 
         }
+
+
 
 
 
