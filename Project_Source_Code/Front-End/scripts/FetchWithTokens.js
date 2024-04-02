d1a3c383fb708541705f4415f4548e0c94e9b722
JesusFranC <jesusfranciscocerda@gmail.com>
2024-04-01 23:17:36 -0700

    Update FetchWithTokens.js

--- Project_Source_Code/Front-End/scripts/FetchWithTokens.js
+++ Project_Source_Code/Front-End/scripts/FetchWithTokens.js
@@ -2,4 +2,11 @@ export function fetchWithTokens( url ) {
     const IDToken = sessionStorage.getItem('IDToken');
     const AccessToken = sessionStorage.getItem('AccessToken');
     const RefreshToken = sessionStorage.getItem('RefreshToken');
+    if (IDToken && AccessToken && RefreshToken) {
+        options.headers = {"Content-Type": "application/json"};
+        options.headers.Authorization = `Bearer ${jwt}`;
+
+      }
+      // Make the HTTP request using fetch
+      return fetch(url, options);
 }
