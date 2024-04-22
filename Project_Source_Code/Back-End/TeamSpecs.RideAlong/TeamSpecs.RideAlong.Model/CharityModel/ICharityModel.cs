eb042435d80df95d9ff6fffb027ea7f93fbc162f
Gio31 <Giovanni.Contreras@student.csulb.edu>
2024-04-21 22:36:38 -0700

    Donate Your Car

---
+++ Project_Source_Code/Back-End/TeamSpecs.RideAlong/TeamSpecs.RideAlong.Model/CharityModel/ICharityModel.cs
@@ -0,0 +1,11 @@
+﻿
+namespace TeamSpecs.RideAlong.Model;
+
+public interface ICharityModel
+{
+    string Name { get; set; }
+    string Description { get; set; }
+    string Link { get; set; }
+
+
+}
