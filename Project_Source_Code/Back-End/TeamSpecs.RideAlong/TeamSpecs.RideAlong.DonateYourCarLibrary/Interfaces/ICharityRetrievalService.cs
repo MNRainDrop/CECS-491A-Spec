dd16f7b57a9b0aa03c75610e4f8dc12421c80dcd
Gio31 <Giovanni.Contreras@student.csulb.edu>
2024-04-27 16:46:48 -0700

    Back-End For DYC

---
+++ Project_Source_Code/Back-End/TeamSpecs.RideAlong/TeamSpecs.RideAlong.DonateYourCarLibrary/Interfaces/ICharityRetrievalService.cs
@@ -0,0 +1,11 @@
+﻿using TeamSpecs.RideAlong.Model;
+
+namespace TeamSpecs.RideAlong.DonateYourCarLibrary.Interfaces
+{
+    public interface ICharityRetrievalService
+    {
+        public IResponse RetrieveCharities(IAccountUserModel userAccount);
+
+
+    }
+}
--- Project_Source_Code/Back-End/TeamSpecs.RideAlong/TeamSpecs.RideAlong.Tests/DonateYourCarTests/DonateYourCarServiceShould.cs
+++ Project_Source_Code/Back-End/TeamSpecs.RideAlong/TeamSpecs.RideAlong.Tests/DonateYourCarTests/DonateYourCarServiceShould.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.Data.SqlClient;
 using System.Diagnostics;
 using TeamSpecs.RideAlong.DataAccess;
+using TeamSpecs.RideAlong.DonateYourCarLibrary;
+using TeamSpecs.RideAlong.LoggingLibrary;
+using TeamSpecs.RideAlong.Model;
+using TeamSpecs.RideAlong.Services;
 
 namespace TeamSpecs.RideAlong.TestingLibrary.DonateYourCarTests
 {
@@ -10,53 +14,67 @@ namespace TeamSpecs.RideAlong.TestingLibrary.DonateYourCarTests
 
         public void DonateYourCar_ReturnCharities()
         {
-            // Arrange
+            //Arrange
             var timer = new Stopwatch();
+
+            IResponse response;
+
             var dao = new SqlServerDAO();
+            var charityTarget = new SqlDbCharityTarget(dao);
 
-            var sql = new SqlCommand("SELECT Charity, Description, Link FROM Charity;");
+            var hashService = new HashService();
+            var logTarget = new SqlDbLogTarget(dao);
+            var logService = new LogService(logTarget, hashService);
 
-            // Expected 
-            var expectedHasError = false;
-            string? expectedErrorMessage = null;
-            var expectedRowCount = 4;
+            var retrievalService = new CharityRetrievalService(charityTarget, logService);
 
-            // Define the expected rows with their values
-            var expectedRows = new List<object[]>
+            var user = new AccountUserModel("testUser")
             {
-                new object[] { "Catholic Charities of Orange County", "The mission of Catholic Charities of Orange County is to serve people in need, strengthen family and community, support parish ministries and extend Gods love to all.", "https://ccoc.careasy.org/home#form-iframe" },
-                new object[] { "Kars4Kids", "Your donation supports the youth and educational programs of national nonprofit Kars4Kids", "https://www.kars4kids.org/" },
-                new object[] { "Vehicles For Veterans", "Your car donation helps benefit disabled veterans throughout the United States. The proceeds from your donation help to fund programs that provide services offered to veterans in need of assistance and support.", "https://www.vehiclesforveterans.org/" },
-                new object[] { "Wheels For Wishes & Wellness", "Wheels For Wishes & Wellness believes that every child, no matter his or her health, deserves a chance to have a happy and fulfilling childhood. By donating your vehicle, we are able to raise funds to support local childrens charities.", "https://kids.wheelsforwishes.org/car-donation-form/" }
+                Salt = 0,
+                UserHash = "testUserHash",
             };
 
-            // Act
-            timer.Start();
-            var response = dao.ExecuteReadOnly(sql);
-            timer.Stop();
 
-            // Assert
-            Assert.True(timer.Elapsed.TotalSeconds <= 3);
-            Assert.True(response.HasError == expectedHasError);
-            Assert.True(response.ErrorMessage == expectedErrorMessage);
-
-            // Assert that the number of rows returned matches the expected row count
-            Assert.Equal(expectedRowCount, response.ReturnValue.Count);
-
-            // Assert that each row in the result matches the expected rows
-            foreach (object[] actualRow in response.ReturnValue)
+            try
             {
-                bool rowFound = false;
-                foreach (object[] expectedRow in expectedRows)
+                var accountSql = $"INSERT INTO UserAccount (UserName, Userhash, Salt) VALUES ('{user.UserName}', '{user.UserHash}', {user.Salt})";
+                dao.ExecuteWriteOnly(new List<KeyValuePair<string, HashSet<SqlParameter>?>>()
                 {
-                    if (actualRow.SequenceEqual(expectedRow))
-                    {
-                        rowFound = true;
-                        break;
-                    }
-                }
-                Assert.True(rowFound, "Row not found in expected rows");
+                    KeyValuePair.Create<string, HashSet<SqlParameter>?>(accountSql, null)
+                });
             }
+            catch
+            {
+                // In case creating the initial sql data does not work
+                var undoInsert = $"DELETE FROM UserAccount WHERE UserHash = '{user.UserHash}'";
+                dao.ExecuteWriteOnly(new List<KeyValuePair<string, HashSet<SqlParameter>?>>()
+                {
+                    KeyValuePair.Create<string, HashSet<SqlParameter>?>(undoInsert, null)
+                });
+            }
+
+            //Act
+            try
+            {
+                timer.Start();
+                response = retrievalService.RetrieveCharities(user);
+                timer.Stop();
+            }
+            finally
+            {
+                var undoInsert = $"DELETE FROM UserAccount WHERE UserHash = ' {user.UserHash}'";
+                dao.ExecuteWriteOnly(new List<KeyValuePair<string, HashSet<SqlParameter>?>>()
+                {
+                    KeyValuePair.Create<string, HashSet<SqlParameter>?>(undoInsert, null)
+                });
+            }
+
+            //Assert
+            Assert.True(timer.Elapsed.TotalSeconds <= 3);
+            Assert.NotNull(response.ReturnValue);
+            Assert.True(response.ReturnValue.Count == 4);
+
         }
     }
 }
+
