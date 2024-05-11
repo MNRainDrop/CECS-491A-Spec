931a187aae94789f45f9406fadccf4c7ef43c7b4
jason barber <jmichael272@yahoo.com>
2024-05-11 01:52:19 -0700

    Skeleton of Controller and Manager

--- Project_Source_Code/Back-End/TeamSpecs.RideAlong/TeamSpecs.RideAlong.UserAdministration/Managers/AccountDeletionManager.cs
+++ Project_Source_Code/Back-End/TeamSpecs.RideAlong/TeamSpecs.RideAlong.UserAdministration/Managers/AccountDeletionManager.cs
@@ -3,10 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TeamSpecs.RideAlong.LoggingLibrary;
+using TeamSpecs.RideAlong.Model;
+using TeamSpecs.RideAlong.SecurityLibrary.Interfaces;
+using TeamSpecs.RideAlong.Services;
 
 namespace TeamSpecs.RideAlong.UserAdministration.Managers
 {
     public class AccountDeletionManager
     {
+        private readonly IAccountDeletionService _accountDeletionService;
+        private readonly ILogService _logService;
+        private readonly IClaimService _claimService;
+
+        public AccountDeletionManager(IAccountDeletionService accountDeletionService, ILogService logService, IClaimService claimService) 
+        {
+            _accountDeletionService = accountDeletionService;
+            _logService = logService;
+            _claimService = claimService;
+        }
+
+        public IResponse DeleteUser(IAccountUserModel model)
+        {
+            IResponse response = new Response();
+
+            // Disoassociate VP's from UID -- Set FleetManagement, MarketpalceStatus, & VendingStatus to default 
+            //implement here
+
+            // Delete BuyRequest, Parts, Listings, Notificaton Center
+            //implement here
+
+            // Delete UserDetails, UserProfile, OTP
+            //implement here
+
+            // Delete all users claims
+            _claimService.DeleteAllUserClaims(model);
+
+            // Delete UserAccount
+            //implement here
+
+            return response;
+        }
     }
 }
