ca50440b83476ecc9cefbfae5abe5934653a982b
Gio31 <Giovanni.Contreras@student.csulb.edu>
2024-05-13 08:03:05 -0700

    Communication Establishment

---
+++ Project_Source_Code/Back-End/TeamSpecs.RideAlong/TeamSpecs.RideAlong.CommEstablishmentLibrary/Interfaces/ISendEmailService.cs
@@ -0,0 +1,11 @@
+﻿
+
+using TeamSpecs.RideAlong.Model;
+
+namespace TeamSpecs.RideAlong.CoEsLibrary.Interfaces
+{
+    public interface ISendEmailService
+    {
+        public IResponse SendEmail(IAccountUserModel accountUser, string vin);
+    }
+}
